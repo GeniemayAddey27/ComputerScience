acd526df8cef23ce66d6d9b642c4f1ac28bbf428
GeniemayAddey27 <832237@mystma.org>
2024-09-26 15:19:11 -0500

    SimpleMadlib

---
+++ Assignments/Simple_mad_lib.py
@@ -0,0 +1,14 @@
+# Once upon a time, in an empty school lab, 
+# there lived a mad scientist. 
+# Every day, the scientist would test experiments happilay.
+# One day, a green cloud appeared
+# and froze the scientist away.
+# The scientist was frozen and scared.
+school = input("Please enter school classroom.\n>")
+scientist = input("Please enter a student profession.\n>")
+action_adverb = input("Please enter an action and adverb.\n>")
+event = input("Please enter a bad event.\n>")
+tragic_accident = input("Please enter a tragic accident.\n>")
+cold = input("Please enter a bad adjective.\n>")
+emotions = input("Please enter the emotions of the accident.\n>")
+print("Once upon a time, in a" + school + ", there lived a " + scientist + ". Every day, the " + scientist + " would " + action_adverb + ". One day, a " + event + " appeared and " + tragic_accident + " the " + scientist + " away. The " + scientist + " was " + cold + " and " + emotions + ".")
