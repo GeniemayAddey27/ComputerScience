e1733cd96a0d3bd2fe31efd7c2c771f89cb4fe6c
GeniemayAddey27 <832237@mystma.org>
2024-09-26 11:16:55 -0500

    Animalsurvey assignment

---
+++ Unit 2 -Functions and Variables/data_conversion.py
@@ -0,0 +1,15 @@
+a = 115         #int -> string
+b = 3.14        #float -> string
+c = "68"        #string -> int
+d = "True"      #string -> boolean
+e = True        #boolean -> string
+f = False       #boolean -> string
+g = '10110111'  #byte -> int
+h = "2.54"      #string -> float
+i = 100         #int -> float
+j = 10.0        #float -> int
+k = 254         #int -> byte
+a_str = str(a)
+print(a_str)
+float_str = str(b) 
+print()
