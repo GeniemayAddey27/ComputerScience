2108a2abebdc5626a21ec9205380a7d1328fc626
GeniemayAddey27 <832237@mystma.org>
2024-09-26 11:12:59 -0500

    Animalsurvey Assignments

---
+++ Assignments/animalsurvey.py
@@ -0,0 +1,5 @@
+input("what is your favorite animal?")
+input("Why do you like them?")
+input("Where do they live?")
+input("What type of sound does this animal make?")
+input("What color is this animal?")
