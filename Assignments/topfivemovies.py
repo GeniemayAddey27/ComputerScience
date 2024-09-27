9c0ea1765ce93695de9b41c0215af92883da3e5e
GeniemayAddey27 <832237@mystma.org>
2024-09-27 15:19:57 -0500

    top5movies

---
+++ Assignments/topfivemovies.py
@@ -0,0 +1,18 @@
+def moviez(a,b,c,d,e):
+    print("Geniemay's Top Five Movies")
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+
+my_function
+
+def add(x, y):
+    print(x + y)
+
+add(5, 10)
+add(2, 2)
+add(15,15)
+add("cat", "dog")
+add("type", "shi")
---
+++ Unit 2 -Functions and Variables/funtions.py
@@ -0,0 +1,8 @@
+x = 5
+
+def my_function():             #Define the function with a name
+    print("Hello World!")
+    print("catdog")
+
+print("Osowski r00ls d00d")
+my_function()
